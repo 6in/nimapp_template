83fb38f4b7cc17deb6c390bede2d675886ad54bc
大矢敏 <ohya@oyabin-MacBook-Pro.local>
2018-04-24 15:55:58 +0900

    add thread sample code

--- src/nimapp_templatepkg/main.nim
+++ src/nimapp_templatepkg/main.nim
@@ -1,8 +1,23 @@
 
 import private/main_impl
 import docopt
+import os
+
+# スレッド処理
+proc threadFunc(param: tuple[a,b:int]) {.thread.} = 
+  for x in param.a..param.b :
+    echo "wait"
+    sleep(1000)
 
 proc main*(args:Table[string,Value]) : int =
   result = 0
   let name = $args["<name>"]
   echo say_hello_to(name)
+
+  # スレッドサンプル
+  var thr: array[0..1, Thread[tuple[a,b:int]]]
+  createThread(thr[0],threadFunc,(1,10))
+  joinThreads(thr)
+
+
+
